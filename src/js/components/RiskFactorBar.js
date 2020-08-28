206b612eef311c163f73ab00c3f7f24e3fb4b85a
joaquin@oktana.io <joaquingimenez83@gmail.com>
2020-08-28 13:29:36 -0300

    styles refactor

--- src/js/components/RiskFactorBar.js
+++ src/js/components/RiskFactorBar.js
@@ -4,7 +4,7 @@ import RiskFactorList from './RiskFactorList';
 function RiskFactorBar(props) {
   return (
     <div className="cell small-12 medium-10">
-      <div className="grid-x risk-label-levels">
+      <div className="grid-x risk-factor--levels">
         <div className="text-left columns small-6">Low</div>
         <div className="text-right columns small-6">High</div>
       </div>
--- src/js/components/RiskFactorHeader.js
+++ src/js/components/RiskFactorHeader.js
@@ -6,7 +6,7 @@ const RiskFactorHeader = () => {
     return (
       <section>
         <h2 className="text-center">Please Select A Risk Level For Your Investment Portfolio</h2>
-        <div className="risk-factor--select grid-x">
+        <div className="risk-factor-select grid-x">
           <RiskFactorBar />
           <RiskFactorBarButton />
         </div>
